392d72c5cf1a6204714709d0cfea197c3b381f92
Mushiurmukul <115237008+Mushiurmukul@users.noreply.github.com>
2023-05-13 21:32:46 +0600

    By type searching added

--- src/main/java/realEstate/InArea.java
+++ src/main/java/realEstate/InArea.java
@@ -46,7 +46,11 @@ public class InArea implements Initializable {
         stage.setScene(scene);
         stage.show();
     }
-
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        locationBox.getItems().addAll("Dhanmondi", "Gulshan", "Uttara", "Motijheel", "Chawkbazar", "Old Dhaka", "Shahbagh", "Mohammadpur", "Mirpur", "Banani", "Baridhara", "Bashundhara", "Nikunja", "Khilgaon", "Panthapath", "Farmgate", "Malibagh", "New Eskaton", "Siddheswari", "Jatrabari", "Wari", "Gendaria", "Mugda", "Kamrangirchar", "Keraniganj", "Demra", "Kadamtali", "Sabujbagh", "Shyampur", "Badda", "Tejgaon", "Basabo", "Basundhara Residential Area", "Shantinagar", "Hazaribagh", "Lalbagh", "Sutrapur", "Ramna", "Palton", "Dakhshinkhan", "Bimanbandar", "Khilkhet", "Nakhalpara", "Mirpur DOHS", "Rampura", "Merul Badda", "Dhaka Cantonment", "Banglamotor", "Gulistan", "Armanitola", "Postogola", "Chak Bazar", "Banga Bazar", "Shampur", "Kalyanpur", "Uttar Khan", "Azimpur", "Nawabganj", "Tongi", "Kafrul", "Mohakhali", "Bijoynagar", "Gandaria Thana", "Hajipara", "Jatrabari Thana", "Sayedabad", "Sukrabad", "Turag", "Wari Thana");
+        districtBox.getItems().addAll("Dhaka", "Barishal", "Rajshahi", "Sylhet", "Mymenshinh", "Chattogram", "Rajbari");
+    }
 
 
     @FXML
